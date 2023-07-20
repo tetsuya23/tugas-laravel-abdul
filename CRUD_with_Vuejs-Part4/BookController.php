215ea8083785e8194f2943ce2bce4c17830b0ec6
abdul1208 <abdulraufagro@gmail.com>
2023-07-20 19:12:03 +0800

    Tugas CRUD_with_Vuejs-Part4

---
+++ CRUD_with_Vuejs-Part4/BookController.php
@@ -0,0 +1,117 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Author;
+use App\Models\Catalog;
+use App\Models\Publisher;
+use App\Models\Book;
+use Illuminate\Http\RedirectResponse;
+use Illuminate\Http\Request;
+use Illuminate\Http\Response;
+
+class BookController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        $publishers = Publisher::all();
+        $authors = Author::all();
+        $catalogs = Catalog::all();
+        return view ('admin.book', compact('publishers', 'authors', 'catalogs'));
+    }
+
+    public function api() 
+    {
+        $books = Book::all();
+
+        return json_encode($books);
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        $this->validate($request,[
+            'isbn' => ['required'],
+            'title' => ['required'],
+            'year' => ['required'],
+            'qty' => ['required'],
+            'price' => ['required'],
+        ]);
+        Book::create($request->all());
+
+        return redirect('books');
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(Book $book)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(Book $book)
+    {
+        // return view('admin.book.edit', compact('book'));
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, Book $book)
+    {
+        // dd($book);
+        // dd($request->all());
+        $this->validate($request, [
+            'isbn' => ['required'],
+            'title' => ['required'],
+            'year' => ['required'],
+            'publisher_id' => ['required'],
+            'author_id' => ['required'],
+            'catalog_id' => ['required'],
+            'qty' => ['required'],
+            'price' => ['required']
+            
+            ]);
+    
+            // $book->update($request->all());
+            $book->update([
+                'isbn' => $request->isbn,
+                'title' => $request->title,
+                'year' => $request->year,
+                'publisher_id' => $request->publisher_id,
+                'author_id' => $request->author_id,
+                'catalog_id' => $request->catalog_id,
+                'qty' => $request->qty,
+                'price' => $request->price
+            ]);
+    
+
+            return redirect('books');
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+
+     public function destroy(Book $book)
+     {
+         $book->delete();
+     }
+}
